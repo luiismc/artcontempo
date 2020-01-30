d2f90b3596a324ebd336ecccf8fe2c751174305e
Luis <luiizmc@gmail.com>
2020-01-30 14:22:49 -0600

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://artcontemporaneo.com/';
+
+export const CONSUMER_KEY = 'ck_22531848e6ec599311f1be27aa900930d014c830';
+export const CONSUMER_SECRET = 'cs_677c5b389b0da8bda537aa56105a5acd64fa39a3';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET
+};
